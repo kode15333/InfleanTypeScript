4d83c68d7a3174a8adf2e017e8040efa11059fcc
Will <kode15333@gmail.com>
2021-03-17 09:14:16 +0900

    TodoApp step1

--- todoApp/src/input.ts
+++ todoApp/src/input.ts
@@ -2,12 +2,13 @@ import readline from 'readline';
 
 const readlineInterface = readline.createInterface({
     input: process.stdin,
-    output: process.stdin,
-})
+    output: process.stdout,
+});
 
 export function waitForInput(msg: string) {
-    return new Promise(res =>
-        readlineInterface.question(msg, name => {
-            res(name);
-        }))
+    return new Promise<string>(res =>
+        readlineInterface.question(msg, key => {
+            res(key);
+        }),
+    );
 }
